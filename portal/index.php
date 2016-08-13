88be82291df735fb106929f330fa937b2a3c10b0
twoutlook <twcloudwebapp@outlook.com>
2016-08-13 13:19:04 +0000

    portal adj

--- portal/index.php
+++ portal/index.php
@@ -55,7 +55,7 @@ if (strlen($active_user) == 0) {
             <div style="font-size: 32pt">
                 <ul>
                     <li>
-                        <a href="../A0707/">RFQ 報價自動化 </a> <span style="font-size: 12pt"> <mark> 版本︰2016-07-07</mark></span>
+                        <a href="../prj201608/">Machine Demands Cloud </a> <span style="font-size: 12pt"> <mark> 版本︰2016-07-07</mark></span>
                     </li>
          
                 </ul>
